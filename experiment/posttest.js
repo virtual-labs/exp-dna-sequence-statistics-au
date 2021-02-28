0732543985a1b34ea543df4fd09696b0e76d1c98
Amrita Mind Brain Center <79529753+amritamindbrain@users.noreply.github.com>
2021-02-28 13:41:33 +0530

    Content added

--- experiment/posttest.js
+++ experiment/posttest.js
@@ -88,23 +88,62 @@
     /////////////// Write the MCQ below in the exactly same described format ///////////////
 
 
-    const myQuestions = [{
-            question: "1.This is a sample question:", ///// Write the question inside double quotes
-            answers: {
-                a: "This is a sample answer A", ///// Write the option 1 inside double quotes
-                b: "This is a sample answer B", ///// Write the option 2 inside double quotes
+    const myQuestions = [
+        
+        
+        {
+            question: "1.Which function is used for finding the GC content in R program ?", ///// Write the question inside double quotes
+            answers: 
+            {
+                a: "GC ( )", ///// Write the option 1 inside double quotes
+                b: "FindGC ( )", ///// Write the option 2 inside double quotes
+                c: "GetGCcontet ( )", ///// Write the option 2 inside double quotes
+                d: "GCratio ( )", ///// Write the option 2 inside double quotes
+
             },
             correctAnswer: "a" ///// Write the correct option inside double quotes
         },
 
-    {
-      question: "<img src='images/8.PNG'><br>Identify the location of Secondary electron detector",  ///// Write the question inside double quotes
-      answers: {
-        a: "<img src='images/1b.png'>",                  ///// Write the option 1 inside double quotes
-        b: "<img src='images/1a.png'>",                  ///// Write the option 2 inside double quotes
-        c: "<img src='images/1c.PNG'>",      },
-      correctAnswer: "c"                ///// Write the correct option inside double quotes
-    },
+        {
+            question: "2.Which library is using for finding GC content in R programing ?", ///// Write the question inside double quotes
+            answers: 
+            {
+                a: "Ses library", ///// Write the option 1 inside double quotes
+                b: "Seqinr library", ///// Write the option 2 inside double quotes
+                c: "Sfsmisc library", ///// Write the option 2 inside double quotes
+                d: "SemTools library", ///// Write the option 2 inside double quotes
+
+            },
+            correctAnswer: "b" ///// Write the correct option inside double quotes
+        },
+
+
+        {
+            question: "3.Which factor of DNA is influenced by its GC content ?", ///// Write the question inside double quotes
+            answers: 
+            {
+                a: "Melting point", ///// Write the option 1 inside double quotes
+                b: "Boiling point", ///// Write the option 2 inside double quotes
+                c: "Elongation", ///// Write the option 2 inside double quotes
+                d: "Replication", ///// Write the option 2 inside double quotes
+
+            },
+            correctAnswer: "a" ///// Write the correct option inside double quotes
+        },
+
+
+        {
+            question: "4.GC content can be calculated using the equation:", ///// Write the question inside double quotes
+            answers: 
+            {
+                a: "( (G+C) / A + T + C + G )) * 100", ///// Write the option 1 inside double quotes
+                b: "( (A+C) / C + G )) * 100", ///// Write the option 2 inside double quotes
+                c: "( (T+C) / C - G + A)) * 100", ///// Write the option 2 inside double quotes
+                d: "( (A+T) / A + T + C + G )) * 100", ///// Write the option 2 inside double quotes
+
+            },
+            correctAnswer: "a" ///// Write the correct option inside double quotes
+        },
 
 
         
